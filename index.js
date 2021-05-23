d7a483ca30a0f856bd654a1aadd072dad7f1eba7
rd205 <rd205@lyrane.cs.st-andrews.ac.uk>
2021-05-23 19:26:58 +0100

    Added help function

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const keepAlive = require('./server.js')
 const language = require('@google-cloud/language')
 const fs = require("fs")
 const fetch = require("node-fetch")
+const schedule = require('node-schedule')
+
+const channelScheduleMap = {}
 
 require('dotenv').config();
 
@@ -22,15 +25,18 @@ client.on('ready', () => {
 })
 
 client.on('message', async (msg) => {
+
+  if (msg.content == '!help'){
+    msg.reply("Hi there. CheerBot is a AI-backed Discord bot that aims to improve the positivity of discord users with the following features: \n\n- `!schedule` sends a positive message in the server everyday at 9am(EST) after being called. This can be stopped by calling `!schedule` again.\n- `!quote` sends an encouragement message.\n- `!dog` sends an image of an adorable puppy to brighten up the user's mood.\n\nAdditionally, all other messages in the server are also screened by the bot using sentiment analysis. Any message that is too negative would cause CheerBot to respond with a message to console the user")
+  }
   
   // gets random quote from API
-  if (msg.content.startsWith('!quote')) {
-    //msg.content == 'ping' other way to test input
+  if (msg.content == '!quote') {
     msg.reply(positivityAPI.random());
   }
 
   // gets random dog image from API
-  else if (msg.content.startsWith('!dog')){
+  else if (msg.content == '!dog'){
     const message = "Hey there. Are you feeling down? What about a random image of a dog to cheer you up. Aren't they adorable?"
     const response = await fetch(API_URL)
     const buffer = await response.buffer()
@@ -39,7 +45,24 @@ client.on('message', async (msg) => {
             files:[IMAGE_FILE_PATH]
         })})
   }
-  
+
+  // schedule positive messages to be received everyday at a specific time
+  else if(msg.content == `!schedule`){
+
+    if (msg.channel in channelScheduleMap){
+      channelScheduleMap[msg.channel].cancel()
+      delete channelScheduleMap[msg.channel]
+      msg.reply(`Got it! CheerBot will no longer sent scheduled messages in the morning`)
+    }
+    else{
+      const job = schedule.scheduleJob({hour:9,tz:"America/Detroit"},function(){
+        const message = "Good morning! The quote of the day is:\n\n"
+        msg.channel.send(message + positivityAPI.random())
+      })
+      channelScheduleMap[msg.channel] = job
+      msg.reply(`Got it! A positive message will be sent in this server everyday at 9.00 EST`)
+    }
+  }
   else{
       const document = {
           content: msg.content,
@@ -49,7 +72,7 @@ client.on('message', async (msg) => {
       const sentiment = result.documentSentiment
 
       if (sentiment.score < SADNESS_THRESHOLD){
-          const reassuring_msg = "Dont be so negative. Perhaps this positive quote would help cheer you up!\n"
+          const reassuring_msg = "Dont be so worried. Perhaps this positive quote would help cheer you up!\n"
           msg.reply(reassuring_msg + '\n' + positivityAPI.random())
       }
   }
